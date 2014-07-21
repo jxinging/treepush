6927f1070e012eed8488195c4043537f8c3affe5
JinXing <jxinging@gmail.com>
2014-07-21 17:34:40 +0800

    加了些文档

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
-#TreePush
+TreePush
 ===========
+一个用于向大量服务器同步文件的推送工具
 
+--------------------
+
+## 实现
+
+1. treepush 根据参数初始化源节点及目标节点列表
+2. treepush 从目标节点列表及源节点列表中各取出一个IP(使用 dip, sip 表示)
+3. 连接到 sip, 执行推送文件到 dip 的命令
+4. 文件推送完成，将 sip, dip 重新添加到源节点列表(多节点并行)
+5. 重复 2~4，直到目标节点列表为空
+
+## 优势
+
+- 随着源节点的增加，更新过程会越来越快，实现更新速度的指数级增涨
+- 使用自定义的文件推送指令，可基于 scp、rsync 进行文件的推送
+- 不需要在所有节点上部署，只需要任意找一台作为控制机安装即可
+
+## 使用示例
+
+```Shell
+# 以 127.0.0.1 为源，使用 rsync 向 hosts.txt 中的目标节点推送文件(已部署 ssh 密钥认证)
+treepush 'rsync -avz -e "ssh -o StrictHostKeyChecking=no -p $TPUSH_PORT" /data/datafile $TPUSH_USER@$TPUSH_HOST:/data/datafile' -s 127.0.0.1 -l hosts.txt
+
+# 指定 hosts.txt 第 2,3,4 列分别为 host,port,user(默认格式为第 1,2,3 列分别对应 host,port,user)
+treepush 'rsync -avz -e "ssh -o StrictHostKeyChecking=no -p $TPUSH_PORT" /data/datafile $TPUSH_USER@$TPUSH_HOST:/data/datafile' -s 127.0.0.1 -l hosts.txt -f 2:host,port,user
+
+# 指定第 4,1,3 列分别为 port,user,host
+treepush 'rsync -avz -e "ssh -o StrictHostKeyChecking=no -p $TPUSH_PORT" /data/datafile $TPUSH_USER@$TPUSH_HOST:/data/datafile' -s 127.0.0.1 -l hosts.txt -f 4:port,1:user,3:host
+
+# 额外命名第 5 列为 path(会生成一个对应的环境变量 TPUSH_PATH)，并根据此变量推送文件
+treepush 'rsync -avz -e "ssh -o StrictHostKeyChecking=no -p $TPUSH_PORT" /data/datafile $TPUSH_USER@$TPUSH_HOST:$TPASH_PATH/datafile' -s 127.0.0.1 -l hosts.txt -f 4:port,1:user,3:host,5:path
+
+# 使用 scp 推送文件
+treepush 'scp -o StrictHostKeyChecking=no -P $TPUSH_PORT /data/datafile $TPUSH_USER@$TPUSH_HOST:/data/datafile' -s 127.0.0.1 -l hosts.txt
+
+# 指定重试次数(-r)和一个源节点的并发连接数(-m)
+treepush 'scp -o StrictHostKeyChecking=no -P $TPUSH_PORT /data/datafile $TPUSH_USER@$TPUSH_HOST:/data/datafile' -s 127.0.0.1 -l hosts.txt -r 3 -m 4
+
+```
+
+### 各选项的说明:
+```text
+  -h, --help            show this help message and exit
+  -d, --debug           开启 debug
+  -l LISTFILE, --list=LISTFILE
+                        目标服务器列表
+  -f FORMAT, --format=FORMAT
+                        指定列表文件各字段的名字，使用 "," 分隔字段名,
+                        连接多个逗号可用于跳过字段指定的字段名会被变成全大写并加上
+                        "TPUSH_"前缀添加到环境变量中。字段名支持使用 "#:<name>"
+                        直接指定第#个字段对应的名字。(host,port,user 这三个字段固定对应: 主机,端口,用户名)
+  -s SOURCE, --source=SOURCE
+                        版本源机器列表(1.1.1.1,2.2.2.2)
+  -m MAX_CONN, --max=MAX_CONN
+                        单IP并发连接数限制(default:4)
+  -r RETRY, --retry=RETRY
+                        单个IP出错后的重试次数(default:0)
+  --logdir=LOGDIR       日志输出目录(default:logs)
+
+```
+
+## 其他
+
+- 测试环境: CentOS 6.3, Python2.7
--- treepush/treepush.py
+++ treepush/treepush.py
@@ -55,32 +55,31 @@ def parse_listfile(list_file, format_str):
 
 EPILOG = ("\n"
 "TIPS:\n"
-"1. 命令中默认可用的环境变量: TPUSH_SRC, TPUSH_HOST, TPUSH_PORT, \n"
-"""2. rsync 命令参考: rsync -avz -e "ssh -o StrictHostKeyChecking=no"\n"""
-"3. scp 命令参考: scp -o StrictHostKeyChecking=no\n"
-"4. 执行过程中可输入命令进行交互控制, 输入 help 查看帮助\n"
-"5. 完整的调用命令参考:\n"
+"1. 固定的环境变量: TPUSH_SRC, TPUSH_HOST, TPUSH_PORT, \n"
+"2. 执行过程中可输入命令进行交互控制, 输入 help 查看帮助\n"
+"3. 完整的调用命令参考:\n"
 """treepush 'rsync -avz -e "ssh -o StrictHostKeyChecking=no -p $TPUSH_PORT" """
-"""/data/datafile TPUSH_HOST:/data/datafile' -r 3 -m 4 -s 1.1.1.1 -l dest_hosts.txt\n"""
-"""treepush 'ssh -o StrictHostKeyChecking=no -p $TPUSH_PORT root@TPUSH_HOST "ip addr show"' """
+"""/data/datafile $TPUSH_HOST:/data/datafile' -r 3 -m 4 -s 1.1.1.1 -l dest_hosts.txt\n"""
+"""treepush 'ssh -o StrictHostKeyChecking=no -p $TPUSH_PORT root@$TPUSH_HOST "ip addr show"' """
 """-r 3 -m 4 -s 1.1.1.1 -l dest_hosts.txt\n\n"""
 ).decode("utf8")
 
 def main():
     from optparse import OptionParser
     OptionParser.format_epilog = lambda self, epilog: self.epilog   # 重写 format_epilog(), 默认的方法会自动去掉换行
-    parser = OptionParser(usage=u"Usage: %prog -l <listfile> -s <source_hosts> [-r|-m|-d] cmd",
+    parser = OptionParser(usage=u"Usage: %prog cmd -l <listfile> -s <source_hosts> [optinos]",
                                 version="version %s" % __VERSION__,
                                 epilog=EPILOG)
     parser.add_option('-d', '--debug', action='store_true', dest='debug', default=False,
                             help=u'开启 debug')
     parser.add_option('-l', '--list', dest='listfile',
                             help=u'目标服务器列表')
-    parser.add_option('-f', '--format', dest='format', default='dest,dest_port',
+    parser.add_option('-f', '--format', dest='format', default='host,port,user',
                             help=(
                                 u'指定列表文件各字段的名字，使用 "," 分隔字段名, 连接多个逗号可用于跳过字段'
-                                u'指定的字段名会被变成全大写并加上 "TPUSH_" 前缀添加到环境变量中.\n'
-                                u'字段名支持使用 "#:<name>" 直接指定第#个字段对应的名字'))
+                                u'指定的字段名会被变成全大写并加上 "TPUSH_"前缀添加到环境变量中。'
+                                u'字段名支持使用 "#:<name>" 直接指定第#个字段对应的名字。'
+                                u'(host,port,user 这三个字段固定对应: 主机,端口,用户名)'))
     parser.add_option('-s', '--source', dest='source',
                             help=u'版本源机器列表(1.1.1.1,2.2.2.2)')
     parser.add_option('-m', '--max', dest='max_conn', type='int', default=4,
